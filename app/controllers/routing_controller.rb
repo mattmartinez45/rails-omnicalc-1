345bb2eb0cbbd6c9ab8801e14a50492a60ac453d
mattmartinez45 <114887556+mattmartinez45@users.noreply.github.com>
2024-04-23 02:49:48 +0000

    original added

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -5,6 +5,8 @@ class RoutingController < ApplicationController
   end
 
   def results_square
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** 2
     render({:template => "pages/results_square"})
   end
 
@@ -13,6 +15,8 @@ class RoutingController < ApplicationController
   end
 
   def results_root
+    @the_num_2 = params.fetch("users_number_2").to_f
+    @the_result_2 = Math.sqrt(@the_num_2)
     render({:template => "pages/results_root"})
   end
 
@@ -21,6 +25,24 @@ class RoutingController < ApplicationController
   end
   
   def results_payment
+    @the_apr = params.fetch("user_apr").to_f
+    @the_apr_percent = @the_apr.to_fs(:percentage, {:precision => 4})
+    @the_years = params.fetch("user_years").to_f
+  
+    @the_r = ((params.fetch("user_apr").to_f)/100)/12
+    @the_n = (params.fetch("user_years").to_f)*12
+    @the_princ = params.fetch("user_pv").to_f
+    @the_principal = @the_princ.to_fs(:currency)
+  
+    @numerator = @the_r * @the_princ
+  
+    @periods = @the_n * -1
+    @changed_r = 1 + @the_r
+    @r_power = @changed_r ** @periods
+    @denominator = 1 - @r_power
+    @the_result_payment = @numerator / @denominator
+    @the_result_payment = @the_result_payment.to_fs(:currency)
+    
     render({:template => "pages/results_payment"})
   end
 
@@ -29,6 +51,9 @@ class RoutingController < ApplicationController
   end
 
   def results_random
+    @min_val = params.fetch("user_min").to_f
+    @max_val = params.fetch("user_max").to_f
+    @rand_output = rand(@min_val..@max_val)
     render({:template => "pages/results_random"})
   end
 
